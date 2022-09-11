abb04a1b7f76c15b3606e8fa52d9cb6b31342475
jschang0215 <2002chang@gmail.com>
2022-09-11 12:53:24 +0900

    2-Definitions

--- 2-Definitions/decl.c
+++ 2-Definitions/decl.c
@@ -8,7 +8,7 @@
 #include "decl.h"
 
 int declarator(int pmtr, int scls, char *name, int *pprim, int *psize,
-		int *pval, int *pinit);
+			   int *pval, int *pinit);
 
 /*
  * enumdecl := { enumlist }
@@ -22,18 +22,21 @@ int declarator(int pmtr, int scls, char *name, int *pprim, int *psize,
  *	| IDENT = constexpr
  */
 
-static void enumdecl(void) {
-	int	v = 0;
-	char	name[NAMELEN+1];
+static void enumdecl(void)
+{
+	int v = 0;
+	char name[NAMELEN + 1];
 
 	Token = scan();
 	if (IDENT == Token)
 		Token = scan();
 	lbrace();
-	for (;;) {
+	for (;;)
+	{
 		copyname(name, Text);
 		ident();
-		if (ASSIGN == Token) {
+		if (ASSIGN == Token)
+		{
 			Token = scan();
 			v = constexpr();
 		}
@@ -41,7 +44,8 @@ static void enumdecl(void) {
 		if (Token != COMMA)
 			break;
 		Token = scan();
-		if (eofcheck()) return;
+		if (eofcheck())
+			return;
 	}
 	match(RBRACE, "'}'");
 	semi();
@@ -57,31 +61,37 @@ static void enumdecl(void) {
  *	| constexpr , const_list
  */
 
-static int initlist(char *name, int prim) {
-	int	n = 0, v;
-	char	buf[30];
+static int initlist(char *name, int prim)
+{
+	int n = 0, v;
+	char buf[30];
 
 	gendata();
 	genname(name);
-	if (STRLIT == Token) {
+	if (STRLIT == Token)
+	{
 		if (PCHAR != prim)
 			error("initializer type mismatch: %s", name);
 		gendefs(Text, Value);
 		gendefb(0);
 		Token = scan();
-		return Value -1;
+		return Value - 1;
 	}
 	lbrace();
-	while (Token != RBRACE) {
+	while (Token != RBRACE)
+	{
 		v = constexpr();
-		if (PCHAR == prim) {
-			if (v < 0 || v > 255) {
+		if (PCHAR == prim)
+		{
+			if (v < 0 || v > 255)
+			{
 				sprintf(buf, "%d", v);
 				error("initializer out of range: %s", buf);
 			}
 			gendefb(v);
 		}
-		else {
+		else
+		{
 			gendefw(v);
 		}
 		n++;
@@ -89,17 +99,19 @@ static int initlist(char *name, int prim) {
 			Token = scan();
 		else
 			break;
-		if (eofcheck()) return 0;
+		if (eofcheck())
+			return 0;
 	}
 	Token = scan();
-	if (!n) error("too few initializers", NULL);
+	if (!n)
+		error("too few initializers", NULL);
 	return n;
 }
 
-int primtype(int t) {
-	return t == CHAR? PCHAR:
-		t == INT? PINT:
-		PVOID;
+int primtype(int t)
+{
+	return t == CHAR ? PCHAR : t == INT ? PINT
+										: PVOID;
 }
 
 /*
@@ -113,25 +125,31 @@ int primtype(int t) {
  *	| primtype declarator , pmtrlist
  */
 
-static int pmtrdecls(void) {
-	char	name[NAMELEN+1];
-	int	prim, type, size, na, y, addr;
-	int	dummy;
+static int pmtrdecls(void)
+{
+	char name[NAMELEN + 1];
+	int prim, type, size, na, y, addr;
+	int dummy;
 
 	if (RPAREN == Token)
 		return 0;
 	na = 0;
-	for (;;) {
-		if (na > 0 && ELLIPSIS == Token) {
+	for (;;)
+	{
+		if (na > 0 && ELLIPSIS == Token)
+		{
 			Token = scan();
 			na = -(na + 1);
 			break;
 		}
-		else if (IDENT == Token) {
+		else if (IDENT == Token)
+		{
 			prim = PINT;
 		}
-		else {
-			if (Token != CHAR && Token != INT && Token != VOID) {
+		else
+		{
+			if (Token != CHAR && Token != INT && Token != VOID)
+			{
 				error("type specifier expected at: %s", Text);
 				Token = synch(RPAREN);
 				return na;
@@ -144,7 +162,7 @@ static int pmtrdecls(void) {
 		}
 		size = 1;
 		type = declarator(1, CAUTO, name, &prim, &size, &dummy,
-				&dummy);
+						  &dummy);
 		addloc(name, prim, type, CAUTO, size, 0, 0);
 		na++;
 		if (COMMA == Token)
@@ -152,24 +170,25 @@ static int pmtrdecls(void) {
 		else
 			break;
 	}
-	addr = INTSIZE*2;
-	for (y = Locs; y < NSYMBOLS; y++) {
+	addr = INTSIZE * 2;
+	for (y = Locs; y < NSYMBOLS; y++)
+	{
 		addr += INTSIZE;
 		Vals[y] = addr;
 	}
 	return na;
 }
 
-int pointerto(int prim) {
+int pointerto(int prim)
+{
 	if (CHARPP == prim || INTPP == prim || VOIDPP == prim ||
-	    FUNPTR == prim
-	)
+		FUNPTR == prim)
 		error("too many levels of indirection", NULL);
-	return PINT == prim? INTPTR:
-		PCHAR == prim? CHARPTR:
-		PVOID == prim? VOIDPTR:
-		INTPTR == prim? INTPP:
-		CHARPTR == prim? CHARPP: VOIDPP;
+	return PINT == prim ? INTPTR : PCHAR == prim ? CHARPTR
+							   : PVOID == prim	 ? VOIDPTR
+							   : INTPTR == prim	 ? INTPP
+							   : CHARPTR == prim ? CHARPP
+												 : VOIDPP;
 }
 
 /*
@@ -188,93 +207,111 @@ int pointerto(int prim) {
  */
 
 int declarator(int pmtr, int scls, char *name, int *pprim, int *psize,
-		int *pval, int *pinit)
+			   int *pval, int *pinit)
 {
-	int	type = TVARIABLE;
-	int	ptrptr = 0;
+	int type = TVARIABLE;
+	int ptrptr = 0;
 
-	if (STAR == Token) {
+	if (STAR == Token)
+	{
 		Token = scan();
 		*pprim = pointerto(*pprim);
-		if (STAR == Token) {
+		if (STAR == Token)
+		{
 			Token = scan();
 			*pprim = pointerto(*pprim);
 			ptrptr = 1;
 		}
 	}
-	else if (LPAREN == Token) {
+	else if (LPAREN == Token)
+	{
 		if (*pprim != PINT)
 			error("function pointers are limited to 'int'",
-				NULL);
+				  NULL);
 		Token = scan();
 		*pprim = FUNPTR;
 		match(STAR, "(*name)()");
 	}
-	if (IDENT != Token) {
+	if (IDENT != Token)
+	{
 		error("missing identifier at: %s", Text);
 		name[0] = 0;
 	}
-	else {
+	else
+	{
 		copyname(name, Text);
 		Token = scan();
 	}
-	if (FUNPTR == *pprim) {
+	if (FUNPTR == *pprim)
+	{
 		rparen();
 		lparen();
 		rparen();
 	}
-	if (!pmtr && ASSIGN == Token) {
+	if (!pmtr && ASSIGN == Token)
+	{
 		Token = scan();
 		*pval = constexpr();
 		if (*pval && !inttype(*pprim))
 			error("non-null pointer initialization", NULL);
 		*pinit = 1;
 	}
-	else if (!pmtr && LPAREN == Token) {
+	else if (!pmtr && LPAREN == Token)
+	{
 		Token = scan();
 		*psize = pmtrdecls();
 		rparen();
 		return TFUNCTION;
 	}
-	else if (LBRACK == Token) {
+	else if (LBRACK == Token)
+	{
 		if (ptrptr)
 			error("too many levels of indirection: %s", name);
 		Token = scan();
-		if (RBRACK == Token) {
+		if (RBRACK == Token)
+		{
 			Token = scan();
-			if (pmtr) {
+			if (pmtr)
+			{
 				*pprim = pointerto(*pprim);
 			}
-			else {
+			else
+			{
 				type = TARRAY;
 				*psize = 1;
-				if (ASSIGN == Token) {
+				if (ASSIGN == Token)
+				{
 					Token = scan();
 					if (!inttype(*pprim))
 						error("initialization of"
-							" pointer array not"
-							" supported",
-							NULL);
+							  " pointer array not"
+							  " supported",
+							  NULL);
 					*psize = initlist(name, *pprim);
 					if (CAUTO == scls)
 						error("initialization of"
-							" local arrays"
-							" not supported: %s",
-							name);
+							  " local arrays"
+							  " not supported: %s",
+							  name);
 					*pinit = 1;
 				}
-				else if (CEXTERN != scls) {
+				else if (CEXTERN != scls)
+				{
 					error("automatically-sized array"
-						" lacking initialization: %s",
-						name);
+						  " lacking initialization: %s",
+						  name);
 				}
 			}
 		}
-		else {
-			if (pmtr) error("array size not supported in "
-					"parameters: %s", name);
+		else
+		{
+			if (pmtr)
+				error("array size not supported in "
+					  "parameters: %s",
+					  name);
 			*psize = constexpr();
-			if (*psize <= 0) {
+			if (*psize <= 0)
+			{
 				error("invalid array size", NULL);
 				*psize = 1;
 			}
@@ -287,20 +324,21 @@ int declarator(int pmtr, int scls, char *name, int *pprim, int *psize,
 	return type;
 }
 
-void signature(int fn, int from, int to) {
-	char	types[MAXFNARGS+1];
-	int	i;
+void signature(int fn, int from, int to)
+{
+	char types[MAXFNARGS + 1];
+	int i;
 
 	if (to - from > MAXFNARGS)
 		error("too many function parameters", Names[fn]);
-	for (i=0; i<MAXFNARGS && from < to; i++)
+	for (i = 0; i < MAXFNARGS && from < to; i++)
 		types[i] = Prims[--to];
 	types[i] = 0;
 	if (NULL == Mtext[fn])
 		Mtext[fn] = globname(types);
 	else if (Sizes[fn] >= 0 && strcmp(Mtext[fn], types))
 		error("redefinition does not match prior type: %s",
-			Names[fn]);
+			  Names[fn]);
 }
 
 /*
@@ -318,53 +356,63 @@ void signature(int fn, int from, int to) {
  *	| declarator , ldecl_list
  */
 
-static int localdecls(void) {
-	char	name[NAMELEN+1];
-	int	prim, type, size, addr = 0, val, ini;
-	int	stat;
-	int	pbase, rsize;
+static int localdecls(void)
+{
+	char name[NAMELEN + 1];
+	int prim, type, size, addr = 0, val, ini;
+	int stat;
+	int pbase, rsize;
 
 	Nli = 0;
-	while (	STATIC == Token ||
-		INT == Token || CHAR == Token || VOID == Token
-	) {
+	while (STATIC == Token ||
+		   INT == Token || CHAR == Token || VOID == Token)
+	{
 		stat = 0;
-		if (STATIC == Token) {
+		if (STATIC == Token)
+		{
 			Token = scan();
 			stat = 1;
-			if (INT == Token || CHAR == Token || VOID == Token) {
+			if (INT == Token || CHAR == Token || VOID == Token)
+			{
 				prim = primtype(Token);
 				Token = scan();
 			}
 			else
 				prim = PINT;
 		}
-		else {
+		else
+		{
 			prim = primtype(Token);
 			Token = scan();
 		}
 		pbase = prim;
-		for (;;) {
+		for (;;)
+		{
 			prim = pbase;
-			if (eofcheck()) return 0;
+			if (eofcheck())
+				return 0;
 			size = 1;
 			ini = val = 0;
 			type = declarator(0, CAUTO, name, &prim, &size,
-					&val, &ini);
+							  &val, &ini);
 			rsize = objsize(prim, type, size);
-			rsize = (rsize + INTSIZE-1) / INTSIZE * INTSIZE;
-			if (stat) {
+			rsize = (rsize + INTSIZE - 1) / INTSIZE * INTSIZE;
+			if (stat)
+			{
 				addloc(name, prim, type, CLSTATC, size,
-					label(), val);
+					   label(), val);
 			}
-			else {
+			else
+			{
 				addr -= rsize;
 				addloc(name, prim, type, CAUTO, size, addr, 0);
 			}
-			if (ini && !stat) {
-				if (Nli >= MAXLOCINIT) {
+			if (ini && !stat)
+			{
+				if (Nli >= MAXLOCINIT)
+				{
 					error("too many local initializers",
-						NULL);
+						  NULL);
 					Nli = 0;
 				}
 				LIaddr[Nli] = addr;
@@ -390,30 +438,36 @@ static int localdecls(void) {
  *	| declarator , decl_list
  */
 
-void decl(int clss, int prim) {
-	char	name[NAMELEN+1];
-	int	pbase, type, size = 0, val, init;
-	int	lsize;
+void decl(int clss, int prim)
+{
+	char name[NAMELEN + 1];
+	int pbase, type, size = 0, val, init;
+	int lsize;
 
 	pbase = prim;
-	for (;;) {
+	for (;;)
+	{
 		prim = pbase;
 		val = 0;
 		init = 0;
 		type = declarator(0, clss, name, &prim, &size, &val, &init);
-		if (TFUNCTION == type) {
-			if (SEMI == Token) {
+		if (TFUNCTION == type)
+		{
+			if (SEMI == Token)
+			{
 				Token = scan();
 				clss = CEXTERN;
 			}
 			Thisfn = addglob(name, prim, type, clss, size, 0,
-					NULL, 0);
+							 NULL, 0);
 			signature(Thisfn, Locs, NSYMBOLS);
-			if (clss != CEXTERN) {
+			if (clss != CEXTERN)
+			{
 				lbrace();
 				lsize = localdecls();
 				gentext();
-				if (CPUBLIC == clss) genpublic(name);
+				if (CPUBLIC == clss)
+					genpublic(name);
 				genname(name);
 				genentry();
 				genstack(lsize);
@@ -425,12 +479,13 @@ void decl(int clss, int prim) {
 				genexit();
 				if (O_debug & D_LSYM)
 					dumpsyms("LOCALS: ", name, Locs,
-						NSYMBOLS);
+							 NSYMBOLS);
 			}
 			clrlocs();
 			return;
 		}
-		if (CEXTERN == clss && init) {
+		if (CEXTERN == clss && init)
+		{
 			error("initialization of 'extern': %s", name);
 		}
 		addglob(name, prim, type, clss, size, val, NULL, init);
@@ -455,14 +510,23 @@ void decl(int clss, int prim) {
  *	| STATIC
  */
 
-void top(void) {
-	int	prim, clss = CPUBLIC;
+void top(void)
+{
+	int prim, clss = CPUBLIC;
 
-	switch (Token) {
-	case EXTERN:	clss = CEXTERN; Token = scan(); break;
-	case STATIC:	clss = CSTATIC; Token = scan(); break;
+	switch (Token)
+	{
+	case EXTERN:
+		clss = CEXTERN;
+		Token = scan();
+		break;
+	case STATIC:
+		clss = CSTATIC;
+		Token = scan();
+		break;
 	}
-	switch (Token) {
+	switch (Token)
+	{
 	case ENUM:
 		enumdecl();
 		break;
